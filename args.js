06a8e0642455bb366fc7424b073961a810563f23
Pranish <pranishpoudel01@gmail.com>
2023-12-30 14:33:43 +0545

    feat: :sparkles: Intro to OOP in JS

--- args.js
+++ args.js
@@ -1,8 +1,23 @@
-function removeFirstTwo(list) {
-    const [a,b, ...arr]=list;
-    
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    // Only change code below this line
+    const failureItems = (arr)=>{
+    for (i=0;i<arr.length;i++){
+        arr[i]= `<li class="text-warning">${arr[i]}</li>\n`
+    }
+    return arr;
+    };
 
+    // Only change code above this line
+  
+    return failureItems(arr);
   }
   
-  const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const sourceWithoutFirstTwo = removeFirstTwo(source);
+  const failuresList = makeList(result.failure);
+
+
+  console.log(failuresList);
