644a744c96ccaccff21359052d68463ae3f455c7
Pranish <pranishpoudel01@gmail.com>
2023-12-31 21:18:51 +0545

    feat: :sparkles: HasownProperty In OOP of JS

---
+++ splitarr.js
@@ -0,0 +1,13 @@
+
+function chunkArrayInGroups(arr, size) {
+    const newarr=[];
+    const arrlength= arr.length;
+    
+    if (arrlength%size===0){
+for (let i=0;i<arr;i++){
+    newarr.push(arr.slice)  
+}
+    }
+  }
+  
+  console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
